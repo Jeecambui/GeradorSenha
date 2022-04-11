d906058118e7b8c280c4e9d3ddf48461c8125a87
Jeecambui <jeferson_cambui@hotmail.com>
2022-04-11 04:50:00 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const generateEl = document.getElementById("generate");
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqestubwxyz";
 const numbers = "0123456789";
-const symbols = "!@#$%¨&*(_)-+=";
+const symbols = "!@#$%¨&*()_=+,.;<>:?/";
 
 function getLowercase(){
     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
@@ -47,7 +47,56 @@ function generatePassword(){
     }
 
     if(symbolEl.checked){
-        password += getSymbal();
+        password += getSymbol();
     }
 
+    for(let i = password.length; i < len; i++){
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+
 }
+
+function generateX(){
+    const xs = [];
+
+    if(upperEl.checked){
+        xs.push(getUppercase());
+    }
+
+    if(lowerEl.checked){
+        xs.push(getLowercase());
+    }
+
+    if(numberEl.checked){
+        xs.push(getNumber());
+    }
+
+    if(symbolEl.checked){
+        xs.push(getSymbol());
+    }
+
+    if(xs.length === 0) return "";
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePassword);
+
+copyEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const password = pwEl.innerText;
+
+    if(!password){
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("o texto foi copiado");
+});
